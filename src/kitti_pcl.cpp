8d322180c2ac928d6a1287f29174ca54789d1844
Simon Appel <simonappel62@gmail.com>
2018-02-10 14:13:52 +0100

    Added dbscan class and dummy box

--- src/kitti_pcl.cpp
+++ src/kitti_pcl.cpp
@@ -2,6 +2,7 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/Point.h>
+#include <visualization_msgs/Marker.h>
 
 // PCL specific includes
 #include <pcl_conversions/pcl_conversions.h>
@@ -9,13 +10,19 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/extract_indices.h>
 
+// Detection
+#include "../include/test_kitti/dbscan.h"
+
 // Publisher
-ros::Publisher pub;
+ros::Publisher pcl_pub;
+ros::Publisher dbb_pub;
 
 // Parameters
 const float voxel_size = 0.2;
-const float opening_angle = M_PI/3;
-const float minimum_height = -1.4;
+const float opening_angle = M_PI/4;
+const float minimum_height = -1.3;
+const float minimum_range = 3.0;
+const float maximum_range = 20.0;
 
 const bool filter_pointcloud = true;
 const bool convert_to_voxelgrid = false;
@@ -45,10 +52,14 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& input){
 
       // Check opening angle
       if(angle < opening_angle){
-        // Check minimum height
-        //if(point.z > minimum_height && angle < opening_angle){
-          inliers->indices.push_back(i);
-        //}
+        // Check range
+        float range = std::sqrt(point.x * point.x + point.y * point.y);
+        if(minimum_range < range && range < maximum_range){
+          // Check minimum height
+          if(point.z > minimum_height && angle < opening_angle){
+            inliers->indices.push_back(i);
+          }
+        }
       }
     }
 
@@ -69,8 +80,39 @@ void callback(const sensor_msgs::PointCloud2ConstPtr& input){
     sor.filter(*cloud);
   }
 
+  // Detector
+  Dbscan detector = Dbscan();
+  // Detection step
+  detector.run_dbscan(cloud);
+
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = "base_link";
+  marker.header.stamp = ros::Time();
+  marker.ns = "my_namespace";
+  marker.id = 0;
+  marker.type = visualization_msgs::Marker::CUBE;
+  marker.action = visualization_msgs::Marker::ADD;
+  marker.pose.position.x = 10;
+  marker.pose.position.y = 0;
+  marker.pose.position.z = 1;
+  marker.pose.orientation.x = 0.0;
+  marker.pose.orientation.y = 0.0;
+  marker.pose.orientation.z = 0.0;
+  marker.pose.orientation.w = 1.0;
+  marker.scale.x = 4;
+  marker.scale.y = 2;
+  marker.scale.z = 2;
+  marker.color.a = 0.3; // Don't forget to set the alpha!
+  marker.color.r = 0.0;
+  marker.color.g = 1.0;
+  marker.color.b = 0.0;
+  //only if using a MESH_RESOURCE marker type:
+  marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
+  dbb_pub.publish(marker);
+
   // Publish the data
-  pub.publish(cloud);
+  std::cout << cloud->size() << std::endl;
+  pcl_pub.publish(cloud);
 }
 
 int main (int argc, char** argv){
@@ -82,7 +124,10 @@ int main (int argc, char** argv){
   ros::Subscriber sub = nh.subscribe ("/kitti/velo/pointcloud", 1, callback);
 
   // Create a ROS publisher for the output point cloud
-  pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+  pcl_pub = nh.advertise<sensor_msgs::PointCloud2> ("pointcloud", 1);
+
+  // Create a ROS publisher for the detected bounding boxes
+  dbb_pub = nh.advertise<visualization_msgs::Marker>( "detection", 0 );
 
   // Spin
   ros::spin ();
