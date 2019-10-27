75f6debdb4925c2a3273b090695817d840ba09d1
appinho <simonappel62@gmail.com>
2019-10-27 17:14:51 +0100

    Removing close duplicated tracks

--- tracking/include/tracking_lib/ukf.h
+++ tracking/include/tracking_lib/ukf.h
@@ -42,7 +42,8 @@ struct Parameter{
 	int tra_aging_bad;
 
 	float tra_occ_factor;
-
+	float tra_min_dist_between_tracks;
+	
 	float p_init_x;
 	float p_init_y;
 	float p_init_v;
@@ -160,9 +161,11 @@ private:
 	float CalculateBoxMismatch(const Track & track, const Object & object);
 	float CalculateEuclideanAndBoxOffset(const Track & track, 
 		const Object & object);
+	float CalculateEuclideanDistanceBetweenTracks(const Track & t1, const Track & t2);
 
 	// Others
 	void setTrackHeight(Track & track, const float h);
+	bool compareGoodAge(Track t1, Track t2);
 
 };
 
